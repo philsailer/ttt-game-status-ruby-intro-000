cd315d294088c6d0831a942ac8c5636fba2698db
Philip Sailer <philsailer@gmail.com>
2020-08-28 00:05:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+def won?(board)
+#  puts board
+#  WIN_COMBINATIONS.each {|combo| puts "yo-#{combo[0]}#{combo[1]}#{combo[2]}=#{board[combo[0]]}#{board[combo[1]]}#{board[combo[2]]}"}
+#  puts WIN_COMBINATIONS.detect{|combo| (board[combo[0]]=="X" || board[combo[0]]=="O") && board[combo[0]]==board[combo[1]] && board[combo[1]]==board[combo[2]]}
+  WIN_COMBINATIONS.detect{|combo| (board[combo[0]]=="X" || board[combo[0]]=="O") && board[combo[0]]==board[combo[1]] && board[combo[1]]==board[combo[2]]}
+end
+
+def full?(board)
+  puts board.detect{|position| position==" "}
+end
